100de9fd5c8f5d3be90e73598b9201850dbbb014
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-02-21 21:21:24 +0200

    Feat: mongoConnect added

---
+++ db/mongoConnect.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.URLDB);
+    console.log("mongo connected");
+}
