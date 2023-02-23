aa39002285f5c7d390b01ae3dc53ea2db3347041
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-02-23 18:54:46 +0200

    Feat: user info req added

--- routes/users.js
+++ routes/users.js
@@ -8,3 +8,15 @@ const router = express.Router();
 router.get("/", async(req,res) => {
     res.json({msg:"Users endpoint"});
 })
+
+
+router.get("/userInfo", auth , async(req,res) => {
+    try{
+        let user = await UserModel.findOne({_id:req.tokenData._id},{password:0})
+        res.json(user)
+    }
+    catch(err){
+        console.log(err);
+        res.status(502).json({err})
+    }
+})
