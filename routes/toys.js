e5cd9fdb5613b1cc9208ef67f401b58ae7862471
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-02-23 13:04:15 +0200

    Feat: post req added

--- routes/toys.js
+++ routes/toys.js
@@ -4,7 +4,11 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
     try {
-        let data = await ToyModel.find({}).limit(10)
+        let data = await ToyModel
+            .find({})
+            .limit(10)
+
+
         res.json(data)
 
     } catch (err) {
@@ -12,5 +16,25 @@ router.get("/", async (req, res) => {
         res.status(502).json(err)
     }
 })
+
+router.post("/", async (req, res) => {
+    let validBody = validateToy(req.body);
+    if (validBody.error) {
+        return res.status(400).json(validBody.error.details);
+    }
+
+    try {
+        let toy = new ToyModel(req.body)
+        await toy.save();
+        res.json(toy)
+
+    } catch (error) {
+        console.log(error)
+        res.status(502).json({error})
+    }
+
+})
+
+
 
 module.exports = router;
