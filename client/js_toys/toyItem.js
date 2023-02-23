bedf2cacbdba88348d43686c4e93080b71c4e888
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-02-23 20:56:06 +0200

    Fix: toyItem added

---
+++ client/js_toys/toyItem.js
@@ -0,0 +1,32 @@
+export default class ToyItem {
+    constructor(_parent, _item, _index, _deleteToy) {
+        this.parent = _parent;
+        this.name = _item.name;
+        this.info = _item.info;
+        this.category = _item.category;
+        this.price = _item.price;
+        this.img_url = _item.img_url;
+        this.id = _item._id;
+        this.index = _index;
+        this.deleteToy = _deleteToy;
+    }
+
+    render() {
+        let div = document.createElement("div");
+        document.querySelector(this.parent).append(div);
+        div.innerHTML = `
+    <td>${this.index + 1}</td>
+    <td>${this.name}</td>
+    <td>${this.info}</td>
+    <td>${this.price} NIS</td>
+    <td>${this.info}</td>
+    <td><button class="bg-danger btn-close x-btn"></button></td>
+    `
+        let xBtn = tr.querySelector(".x-btn");
+        xBtn.addEventListener("click", () => {
+            window.confirm("Delete toy?") && this.deleteToy(this.id)
+        })
+    }
+
+
+}
