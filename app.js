065a17807a9017ec46d826012a772ab478b10491
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-02-27 18:40:38 +0200

    Fix: syntax corrected

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-const express = require("express")
-const http = require("http")
-const path = require("path")
-const cors = require("cors")
+const express = require("express");
+const http = require("http");
+const path = require("path");
+require("dotenv").config();
+// const cors  = require("cors"); мидлвер
 
-const {routesInit} = require("./routes/configRoutes")
+const {routesInit} = require("./routes/configRoutes");
+require("./db/mongoConnect");
 
-require("./db/mongoConnect")
+const app = express();
+// app.use(cors()); мидлвер
+app.use(express.json()); //мидлвер
+app.use(express.static(path.join(__dirname,"public")));
 
-const app = express()
+routesInit(app);
 
-app.use(cors())
+const server = http.createServer(app);
 
-app.use(express.json)
-
-app.use(express.static(path.join(__dirname,"public")))
-
-routesInit(app)
-
-const server = http.createServer(app)
-
-let port = process.env.PORT || 3001
-
-server.listen(port)
+let port = process.env.PORT || 3001;
+server.listen(port);
--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+const config = require('../config/secret').config
+
+const mongoURL = `mongodb+srv://${config.mongoUser}:${config.mongoPass}@cluster0.zg221i8.mongodb.net/fullstack23`
 
 main().catch(err => console.log(err));
 
 async function main() {
     mongoose.set('strictQuery', false);
-    await mongoose.connect(process.env.URLDB);
+    await mongoose.connect(mongoURL);
     console.log("mongo connected");
 }
